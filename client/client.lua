a19c4b38b3dc67864dcf16f4c894509f7efbf888
Haroki <rbtomas89@gmail.com>
2024-12-25 12:33:57 +0200

    Update(Fixes): Some small changes.

--- client/client.lua
+++ client/client.lua
@@ -1,17 +1,57 @@
 -- internal variables
-local hasAlreadyEnteredMarker, isInATMMarker, menuIsShowed = false, false, false
+local State = {
+	uiOpen = false,
+	inMarker = false,
+}
 
-RegisterNetEvent('esx_atm:closeATM')
-AddEventHandler('esx_atm:closeATM', function()
-	SetNuiFocus(false)
-	SendNUIMessage({
-		show = false;
-	})
-	menuIsShowed = false
-end)
+local function openUI()
+	if State.uiOpen then
+		State.uiOpen = false
+		
+		SetNuiFocus(false, false)
+		SendNUIMessage({
+			show = false
+		})
+	else
+		State.uiOpen = true
+		SetNuiFocus(true, true)
 
+		ESX.TriggerServerCallback('h-banking:getAccounts', function(wallet, bank)
+			SendNUIMessage({
+				show = true,
+				wallet = wallet.wallet,
+				bank = wallet.bank
+			})
+		end)
+	end
+end
+
+function NearbyATM()
+	local coords = GetEntityCoords(PlayerPedId())
+
+	for i = 1, #Config.ATMLocations do
+		local atmCoords = vector3(Config.ATMLocations[i].x, Config.ATMLocations[i].y, Config.ATMLocations[i].z)
+		if #(coords - atmCoords) < 1.0 then
+			return true
+		end
+	end
+
+	return false
+end
+
+RegisterCommand('openatm', function()
+    local playerPed = PlayerPedId()
+
+    if not State.uiOpen and IsPedOnFoot(playerPed) and NearbyATM() then
+		openUI()
+    end
+end, false)
+
+RegisterKeyMapping('openatm', 'Open ATM', 'keyboard', 'e')
+
+-- UI Stuff
 RegisterNUICallback('escape', function(data, cb)
-	TriggerEvent('esx_atm:closeATM')
+	openUI()
 end)
 
 RegisterNUICallback('deposit', function(data, cb)
@@ -65,59 +105,20 @@ Citizen.CreateThread(function()
 	end
 end)
 
--- Activate menu when player is inside marker
+-- -- Activate menu when player is inside marker
 if Config.Prompt then
-	Citizen.CreateThread(function()
+	local PromptSleep = 500
+
+	CreateThread(function()
 		while true do
-			Citizen.Wait(50)
-			local letSleep = true
-
-			if IsNearbyATM() then
-				letSleep = false
+			if NearbyATM() then
+				PromptSleep = 5
 				ESX.ShowHelpNotification("Press ~b~E~b~ to open ATM")
+			else
+				PromptSleep = 500
 			end
 
-			if letSleep then 
-				Citizen.Wait(500)
-			end
+			Wait(PromptSleep)
 		end
 	end)
 end
-
-RegisterCommand('openatm', function()
-    local playerPed = PlayerPedId()
-
-    if not menuIsShowed and IsPedOnFoot(playerPed) and IsNearbyATM() then
-    	menuIsShowed = true
-		ESX.TriggerServerCallback('h-banking:getAccounts', function(wallet, bank)
-			SendNUIMessage({
-				show = true,
-				wallet = wallet.wallet,
-				bank = wallet.bank
-			})
-		end)
-		SetNuiFocus(true, true)
-    end
-end, false)
-RegisterKeyMapping('openatm', 'Open ATM', 'keyboard', 'e')
-
-function IsNearbyATM()
-	local coords = GetEntityCoords(PlayerPedId())
-
-	for i = 1, #Config.ATMLocations do
-		local atmCoords = vector3(Config.ATMLocations[i].x, Config.ATMLocations[i].y, Config.ATMLocations[i].z)
-		if #(coords - atmCoords) < 1.0 then
-			return true
-		end
-	end
-	return false
-end
-
--- close the menu when script is stopping to avoid being stuck in NUI focus
-AddEventHandler('onResourceStop', function(resource)
-	if resource == GetCurrentResourceName() then
-		if menuIsShowed then
-			TriggerEvent('esx_atm:closeATM')
-		end
-	end
-end)
